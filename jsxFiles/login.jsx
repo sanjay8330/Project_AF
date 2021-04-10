880b2e5f9d97713e5267eddaf0cc1e5a0ca8483a
sanjay8330 <sanjayabi8330@gmail.com>
2021-04-10 07:36:43 +0530

    Initial Commit

---
+++ jsxFiles/login.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {render} from 'react-dom';
+import '../css/first.css';
+
+render(
+    <div>
+        <div class="topnav">
+            <a class="active" href="#home">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+        </div>
+    </div>,
+    document.getElementById("app")
+)
